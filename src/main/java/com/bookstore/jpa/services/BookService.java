4c1e92d7683f9db82ba1f3c95d6a271195e2272c
Giovani <giovani.aquino1612@gmail.com>
2026-04-15 18:59:40 -0300

    ADD: Controller e Service do Book, adição apenas

---
+++ src/main/java/com/bookstore/jpa/services/BookService.java
@@ -0,0 +1,42 @@
+package com.bookstore.jpa.services;
+
+import com.bookstore.jpa.dtos.BookRecordDto;
+import com.bookstore.jpa.models.BookModel;
+import com.bookstore.jpa.models.ReviewModel;
+import com.bookstore.jpa.repositories.AuthorRepository;
+import com.bookstore.jpa.repositories.BookRepository;
+import com.bookstore.jpa.repositories.PublisherRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
+    private PublisherRepository publisherRepository;
+
+    public BookService(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    // Transactional so only changes when everything works
+    @Transactional
+    public BookModel saveBook(BookRecordDto bookRecordDto){
+        BookModel book = new BookModel();
+        book.setTitle(bookRecordDto.title());
+        book.setPublisher(publisherRepository.findById(bookRecordDto.publisherId()).get());
+        book.setAuthors(authorRepository.findAllById(bookRecordDto.authorId()).stream().collect(Collectors.toSet()));
+
+        ReviewModel reviewModel = new ReviewModel();
+        reviewModel.setComment(bookRecordDto.reviewComment());
+        reviewModel.setBook(book);
+        book.setReview(reviewModel);
+
+        return bookRepository.save(book);
+    }
+}

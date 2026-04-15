b32fec6752e28f258b6097082d6c031910649db6
Giovani <giovani.aquino1612@gmail.com>
2026-04-15 18:12:59 -0300

    ADD: Relações OneToOne na DB

--- src/main/java/com/bookstore/jpa/models/BookModel.java
+++ src/main/java/com/bookstore/jpa/models/BookModel.java
@@ -25,11 +25,14 @@ public class BookModel implements Serializable {
 
     @ManyToMany
     @JoinTable(
-            name = "tb_book_author",
+            name = "book_author",
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id")
     )
     private Set<AuthorModel> authors = new HashSet<>();
+
+    @OneToOne(mappedBy = "book", cascade = CascadeType.ALL)
+    private ReviewModel review;
 
     public UUID getId() {
         return id;
@@ -61,5 +64,13 @@ public class BookModel implements Serializable {
 
     public void setAuthors(Set<AuthorModel> authors) {
         this.authors = authors;
+    }
+
+    public ReviewModel getReview() {
+        return review;
+    }
+
+    public void setReview(ReviewModel review) {
+        this.review = review;
     }
 }
